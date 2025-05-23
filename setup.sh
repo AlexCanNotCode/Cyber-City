97831be91a4bcb872cf02f9acd198c8dc9811ece
Alex Alvarez <alexalvarezbusiness24@gmail.com>
2025-05-23 10:53:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,14 @@
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
+
+cd web-app/terraform
+terraform init                # Initialize Terraform
+terraform plan                # Optional: preview what will be created
+terraform apply    
+
+API_URL=$(terraform output -raw api_url)
+sed -i "s|https://PLACEHOLDER_API_URL|$API_URL|g" ../web/script.js
+
+docker build -t waittime-web .
+docker run -p 80:80 waittime-web
