5ab8044d72973c51c02f8e98113edad1eb141327
Alex Alvarez <alexalvarezbusiness24@gmail.com>
2025-05-23 10:46:08 -0400

    Update main.tf

--- web-app/terraform/main.tf
+++ web-app/terraform/main.tf
@@ -28,6 +28,16 @@ resource "aws_lambda_function" "wait_time" {
   source_code_hash = filebase64sha256("${path.module}/lambda.zip")
 }
 
+resource "aws_lambda_function" "wait_time" {
+  function_name = "wait-time-lambda"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.11"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = filebase64sha256(data.archive_file.lambda_zip.output_path)
+}
+
 resource "aws_api_gateway_rest_api" "api" {
   name = "WaitTimeAPI"
 }
@@ -43,6 +53,59 @@ resource "aws_api_gateway_method" "get_wait" {
   resource_id   = aws_api_gateway_resource.wait.id
   http_method   = "GET"
   authorization = "NONE"
+}
+
+resource "aws_dynamodb_table" "cybercrowds" {
+  name           = "CyberCrowds"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "GuestID"
+  range_key      = "Timestamp"
+
+  attribute {
+    name = "GuestID"
+    type = "S"
+  }
+
+  attribute {
+    name = "Timestamp"
+    type = "N"
+  }
+}
+
+# IAM role for Lambda execution
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Attach basic Lambda and DynamoDB permissions
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamo_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBReadOnlyAccess"
+}
+
+# Archive the lambda function from local file
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/lambda_function.py"
+  output_path = "${path.module}/lambda/lambda_function.zip"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
