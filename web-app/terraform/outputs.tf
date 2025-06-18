6a63ea8f4459327de812695744dd0ad40aecbc64
Alex Alvarez <alexalvarezbusiness24@gmail.com>
2025-06-18 15:51:49 -0400

    Update outputs.tf

--- web-app/terraform/outputs.tf
+++ web-app/terraform/outputs.tf
@@ -1,8 +1,3 @@
 output "api_url" {
-  value = format(
-    "https://%s.execute-api.%s.amazonaws.com/%s/wait",
-    aws_api_gateway_rest_api.api.id,
-    "us-east-1",
-    aws_api_gateway_stage.prod.stage_name
-  )
+  value = "${aws_api_gateway_deployment.deployment.invoke_url}/wait"
 }
