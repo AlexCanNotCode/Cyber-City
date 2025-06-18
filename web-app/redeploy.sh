6e1a5290d908b0ea6f374499d0e91b180ee0d30c
Alex Alvarez <alexalvarezbusiness24@gmail.com>
2025-06-18 14:06:05 -0400

    Update redeploy.sh

--- web-app/redeploy.sh
+++ web-app/redeploy.sh
@@ -4,18 +4,18 @@ AWS_REGION="us-east-1"
 REPO_NAME="waittime-web"
 IMAGE_TAG="latest"
 
-echo "🔁 Rebuilding and pushing Docker image..."
+echo "Rebuilding and pushing Docker image..."
 docker build -t $REPO_NAME .
 ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 ECR_URL="$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME"
 docker tag $REPO_NAME:$IMAGE_TAG $ECR_URL:$IMAGE_TAG
 docker push $ECR_URL:$IMAGE_TAG
 
-echo "🔁 Forcing ECS redeploy..."
+echo "Forcing ECS redeploy..."
 aws ecs update-service \
   --cluster waittime-cluster \
   --service waittime-web-service \
   --region $AWS_REGION \
   --force-new-deployment
 
-echo "✅ Done! New code is live."
+echo "Done! New code is live."
