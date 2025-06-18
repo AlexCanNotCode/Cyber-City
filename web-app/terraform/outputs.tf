c42b4cb9a657f65c525c2c6a4994e651910f7a2a
Alex Alvarez <alexalvarezbusiness24@gmail.com>
2025-06-18 16:16:30 -0400

    Update outputs.tf

--- web-app/terraform/outputs.tf
+++ web-app/terraform/outputs.tf
@@ -1,3 +1,9 @@
 output "api_url" {
-  value = "${aws_api_gateway_deployment.deployment.invoke_url}/wait"
+  description = "Invoke URL for the GET /wait endpoint"
+  value = format(
+    "https://%s.execute-api.%s.amazonaws.com/%s/wait",
+    aws_api_gateway_rest_api.api.id,
+    "us-east-1",                            # Update this if you're in a different AWS region
+    aws_api_gateway_stage.prod.stage_name
+  )
 }
